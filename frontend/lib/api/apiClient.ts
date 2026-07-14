9a1cdac69e3addcb8614d65d1d3c344e43e73e24
Retesh <opboy4885@gmail.com>
2026-07-15 02:05:55 +0530

    debugging the env errors

--- frontend/lib/api/apiClient.ts
+++ frontend/lib/api/apiClient.ts
@@ -1,5 +1,11 @@
 import axios from "axios";
 
+console.log(
+  "NEXT_PUBLIC_BACKEND_URL:",
+  process.env.NEXT_PUBLIC_BACKEND_URL
+);
+
+
 export const apiClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
   withCredentials: true,
