4831b1c50fa2420cc0aa3e00b657b8a54537d55b
Retesh <opboy4885@gmail.com>
2026-07-15 08:00:38 +0530

    fixremoved the console.log apiClient

--- frontend/lib/api/apiClient.ts
+++ frontend/lib/api/apiClient.ts
@@ -1,10 +1,5 @@
 import axios from "axios";
 
-console.log(
-  "NEXT_PUBLIC_BACKEND_URL:",
-  process.env.NEXT_PUBLIC_BACKEND_URL
-);
-
 
 export const apiClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
